e63152cf488dbbe6cf1f7e738b8df1c7e4f03d9f
prog4th <prog4th@proton.me>
2026-01-07 04:40:49 +0900

    🎉프로젝트 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20260000/spring02project/Spring02ProjectApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20260000.spring02project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring02ProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring02ProjectApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20260000/spring02project/Spring02ProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20260000.spring02project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring02ProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
